e05ee1d939698dd655eb05ef280e660e4f0f482d
Josh Brunton <josh.brunton@proton.me>
2025-12-03 16:22:29 +0000

    Carry dynamics service metadata down to operations

--- src/DynamicsEndpointDiscovery.Application/Types/Dynamics/DynSvc.cs
+++ src/DynamicsEndpointDiscovery.Application/Types/Dynamics/DynSvc.cs
@@ -1,7 +1,10 @@
-﻿namespace DynamicsEndpointDiscovery.Application.Types;
+﻿using Newtonsoft.Json;
+
+namespace DynamicsEndpointDiscovery.Application.Types;
 
 public class DynSvc
 {
+    [JsonIgnore] public string ServiceGroupName { get; set; } = string.Empty;
     public string Name { get; set; } = string.Empty;
     public DynSvcOp[] Operations { get; set; } = [];
 }
--- src/DynamicsEndpointDiscovery.Application/Types/Dynamics/DynSvcOp.cs
+++ src/DynamicsEndpointDiscovery.Application/Types/Dynamics/DynSvcOp.cs
@@ -1,7 +1,11 @@
-﻿namespace DynamicsEndpointDiscovery.Application.Types;
+﻿using Newtonsoft.Json;
 
-public class DynSvcOp 
+namespace DynamicsEndpointDiscovery.Application.Types;
+
+public class DynSvcOp
 {
+    [JsonIgnore] public string ServiceGroupName { get; set; } = string.Empty;
+    [JsonIgnore] public string ServiceName { get; set; } = string.Empty;
     public string Name { get; set; } = string.Empty;
     public DynSvcMethodSymbol[] Parameters { get; set; } = [];
     public required DynSvcMethodSymbol Return { get; set; } 
