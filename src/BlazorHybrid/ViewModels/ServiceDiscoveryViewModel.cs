4080ac45d57b8fa49dc4ec4a5224b196f8586c50
Josh Brunton <josh.brunton@proton.me>
2026-01-06 10:42:05 +0000

    Add validation

--- src/BlazorHybrid/ViewModels/ServiceDiscoveryViewModel.cs
+++ src/BlazorHybrid/ViewModels/ServiceDiscoveryViewModel.cs
@@ -1,11 +1,14 @@
-﻿using System.Text;
-using BlazorHybrid.Components;
+﻿using BlazorHybrid.Components;
 using BlazorHybrid.Extensions.Microsoft.FluentUi.AspNetCore.Components;
+using CommunityToolkit.Maui.Storage;
 using Dev.JoshBrunton.DynamicsEndpointDiscovery.Application.Enums;
 using Dev.JoshBrunton.DynamicsEndpointDiscovery.Application.Requests;
 using Dev.JoshBrunton.DynamicsEndpointDiscovery.Application.Services;
-using CommunityToolkit.Maui.Storage;
+using Microsoft.AspNetCore.Components;
 using Microsoft.FluentUI.AspNetCore.Components;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
 
 namespace BlazorHybrid.ViewModels;
 
@@ -21,9 +24,9 @@ internal class ServiceDiscoveryViewModel : IServiceDiscoveryViewModel
     public string Resource { get; set; } = string.Empty;
     public string TokenRequestEndpoint { get; set; } = string.Empty;
 
-    public string GrepGroups { get; set; } = ".*";
-    public string GrepServices { get; set; } = ".*";
-    public string GrepOperations { get; set; } = ".*";
+    public string GrepGroups { get; set; } = string.Empty;
+    public string GrepServices { get; set; } = string.Empty;
+    public string GrepOperations { get; set; } = string.Empty;
 
     public string CollectionName { get; set; } = "Collection";
     public OutputSchemas OutputSchema { get; set; }
@@ -52,15 +55,20 @@ internal class ServiceDiscoveryViewModel : IServiceDiscoveryViewModel
 
         try
         {
+            if (!await ValidateInput())
+            {
+                return;
+            }
+
             string output = await _mainService.GetServiceCollectionAsync(new GetServiceCollectionRequest
             {
                 ClientId = ClientId,
                 ClientSecret = ClientSecret,
                 Resource = Resource,
                 TokenRequestEndpoint = TokenRequestEndpoint,
-                GrepGroupsRegex = GrepGroups,
-                GrepServicesRegex = GrepServices,
-                GrepOperationsRegex = GrepOperations,
+                GrepGroupsRegex = string.IsNullOrWhiteSpace(GrepGroups) ? ".*" : GrepGroups,
+                GrepServicesRegex = string.IsNullOrWhiteSpace(GrepServices) ? ".*" : GrepServices,
+                GrepOperationsRegex = string.IsNullOrWhiteSpace(GrepOperations) ? ".*" : GrepOperations,
                 CollectionName = CollectionName,
                 OutputSchema = OutputSchema,
                 OutputFormat = OutputFormat,
@@ -97,6 +105,77 @@ internal class ServiceDiscoveryViewModel : IServiceDiscoveryViewModel
             IsLoading = false;
             _singleOperationSemaphore.Release();
         }
+    }
+
+    private async Task<bool> ValidateInput()
+    {
+        List<string> errors = [];
+
+        if (!Uri.TryCreate(TokenRequestEndpoint, UriKind.Absolute, out _))
+        {
+            errors.Add("Token request endpoint must be a valid absolute URI");
+        }
+
+        if (!Uri.TryCreate(Resource, UriKind.Absolute, out _))
+        {
+            errors.Add("Resource must be a valid absolute URI");
+        }
+
+        if (!Guid.TryParse(ClientId, out _))
+        {
+            errors.Add("Client ID must be a valid GUID");
+        }
+
+        if (!string.IsNullOrWhiteSpace(GrepGroups))
+        {
+            try
+            {
+                _ = Regex.Match(string.Empty, GrepGroups);
+            }
+            catch (ArgumentException)
+            {
+                errors.Add($"\"{GrepGroups}\" (groups) is not a valid regular expression.");
+            }
+        }
+
+        if (!string.IsNullOrWhiteSpace(GrepOperations))
+        {
+            try
+            {
+                _ = Regex.Match(string.Empty, GrepOperations);
+            }
+            catch (ArgumentException)
+            {
+                errors.Add($"\"{GrepOperations}\" (operations) is not a valid regular expression.");
+            }
+        }
+
+        if (!string.IsNullOrWhiteSpace(GrepServices))
+        {
+            try
+            {
+                _ = Regex.Match(string.Empty, GrepServices);
+            }
+            catch (ArgumentException)
+            {
+                errors.Add($"\"{GrepServices}\" (services) is not a valid regular expression.");
+            }
+        }
+
+        if (errors.Count > 0)
+        {
+            await _dialogService.ShowComponentAsync(new TextComponent
+            {
+                Text = new MarkupString(string.Join(string.Empty, errors.Select(x => $"- {x}<br />")))
+            },
+                new DialogParameters
+                {
+                    Title = "Invalid request"
+                });
+            return false;
+        }
+
+        return true;
     }
 }
 
