95482ff30d3bbd2ae541ce9d5236edd7bfaf080a
Josh Brunton <josh.brunton@proton.me>
2026-01-08 13:17:23 +0000

    Handle crash on bad credentials

--- src/BlazorHybrid/ViewModels/SelectGroupsViewModel.cs
+++ src/BlazorHybrid/ViewModels/SelectGroupsViewModel.cs
@@ -2,13 +2,16 @@
 using BlazorHybrid.Extensions.System.Collections.ObjectModel;
 using Dev.JoshBrunton.DynamicsEndpointDiscovery.Application.Requests;
 using Dev.JoshBrunton.DynamicsEndpointDiscovery.Application.Services;
+using Dev.JoshBrunton.DynamicsEndpointDiscovery.Core.Extensions.Serilog;
 using Dev.JoshBrunton.DynamicsEndpointDiscovery.Lib.Ax.Types;
+using Serilog;
 
 namespace BlazorHybrid.ViewModels;
 
 internal class SelectGroupsViewModel : ISelectGroupsViewModel
 {
     private readonly IMainService _mainService;
+    private readonly ILogger _logger;
 
     public ObservableCollection<Selectable<DynSvcGroup>> ServiceGroups { get; } = [];
     public string Query { get; set; } = string.Empty;
@@ -25,22 +28,30 @@ internal class SelectGroupsViewModel : ISelectGroupsViewModel
         }
     }
 
-    public SelectGroupsViewModel(IMainService mainService)
+    public SelectGroupsViewModel(IMainService mainService, ILogger logger)
     {
         _mainService = mainService;
+        _logger = logger;
     }
 
     public async Task InitAsync(ICredentialsViewModel credentials)
     {
-        var groups = await _mainService.GetAllGroups(new GetAllGroupsRequest
+        try
         {
-            ClientId = credentials.ClientId,
-            ClientSecret = credentials.ClientSecret,
-            Resource = credentials.ResourceUri,
-            TokenRequestEndpoint = credentials.TokenRequestEndpoint
-        });
+            var groups = await _mainService.GetAllGroups(new GetAllGroupsRequest
+            {
+                ClientId = credentials.ClientId,
+                ClientSecret = credentials.ClientSecret,
+                Resource = credentials.ResourceUri,
+                TokenRequestEndpoint = credentials.TokenRequestEndpoint
+            });
 
-        ServiceGroups.ReplaceRange(groups.Select(x => new Selectable<DynSvcGroup>(x)));
+            ServiceGroups.ReplaceRange(groups.Select(x => new Selectable<DynSvcGroup>(x)));
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError("Something went wrong while getting groups. {message}", ex.Message);
+        }
     }
 }
 
