5a507ae309beb6d62cb91f862ee7c5eebc03bc53
Josh Brunton <josh.brunton@proton.me>
2026-01-08 11:09:05 +0000

    Add tests for command+option descriptions

---
+++ test/Cli.Tests/Generators/Commands/CommandGenerator.cs
@@ -0,0 +1,29 @@
+﻿using Dev.JoshBrunton.DynamicsEndpointDiscovery.Cli.Ioc;
+using Microsoft.Extensions.DependencyInjection;
+using System.CommandLine;
+using Dev.JoshBrunton.DynamicsEndpointDiscovery.Cli.Commands;
+
+namespace Dev.JoshBrunton.DynamicsEndpointDiscovery.Tests.Cli.Tests.Generators.Commands;
+
+internal static class CommandGenerator
+{
+    public static IEnumerable<object[]> GetCommands()
+    {
+        ServiceCollection sc = new ServiceCollection();
+        CliServiceRegistrar.RegisterServices(sc);
+        var provider = sc.BuildServiceProvider();
+
+        var rootCommand = provider.GetRequiredService<DynSvcDiscoveryRootCommand>();
+        var stack = new Stack<Command>();
+        stack.Push(rootCommand);
+        while (stack.Count != 0)
+        {
+            var next = stack.Pop();
+            yield return [next];
+            foreach (var child in next.Subcommands)
+            {
+                stack.Push(child);
+            }
+        }
+    }
+}
---
+++ test/Cli.Tests/Tests/Options/OptionTests.cs
@@ -0,0 +1,18 @@
+﻿using System.CommandLine;
+using Dev.JoshBrunton.DynamicsEndpointDiscovery.Tests.Cli.Tests.Generators.Options;
+
+namespace Dev.JoshBrunton.DynamicsEndpointDiscovery.Tests.Cli.Tests.Tests.Options;
+
+[TestClass]
+public class OptionTests
+{
+    [TestMethod]
+    [DynamicData(nameof(OptionsGenerator.GetOptions), typeof(OptionsGenerator))]
+    public void Option_ShouldHaveDescription(Option option)
+    {
+        if (string.IsNullOrWhiteSpace(option.Description))
+        {
+            Assert.Fail($"{option.GetType().FullName} must have a description.");
+        }
+    }
+}
