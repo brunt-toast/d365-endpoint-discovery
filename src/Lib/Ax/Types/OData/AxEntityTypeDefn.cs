d4339962baf094ee7ff0b1b22eb99686d34b64f5
Josh Brunton <josh.brunton@proton.me>
2026-02-02 16:09:18 +0000

    Add common interface for type definitions

--- src/Lib/Ax/Types/OData/AxEntityTypeDefn.cs
+++ src/Lib/Ax/Types/OData/AxEntityTypeDefn.cs
@@ -1,13 +1,27 @@
-﻿using System.Xml.Linq;
+﻿using Dev.JoshBrunton.DynamicsEndpointDiscovery.Lib.Ax.Interfaces;
+using System.Xml.Linq;
 
 namespace Dev.JoshBrunton.DynamicsEndpointDiscovery.Lib.Ax.Types.OData;
 
-public class AxEntityTypeDefn
+public class AxEntityTypeDefn : ITypeDefn
 {
     public required string Name { get; init; }
     public required string[] Keys { get; init; }
-    public required AxEntityTypePropertyDefn[] Properties { get; init; }
+    public required IPropertyDefn[] Properties { get; init; }
     public required AxEntityTypeAnnotationDefn[] Annotations { get; init; }
+
+    public Dictionary<string, object> GetDefault()
+    {
+        return Properties.Select(x => new KeyValuePair<string, object>(x.Name, x.Type switch
+        {
+            "array" => Array.Empty<object>(),
+            "number" => int.MaxValue,
+            "boolean" => false,
+            "string" => string.Empty,
+
+            _ => $"[Unknown type {x.Type}]"
+        })).DistinctBy(x => x.Key).ToDictionary();
+    }
 
     public static IEnumerable<AxEntityTypeDefn> FromODataMetadata(XContainer document)
     {
@@ -18,7 +32,7 @@ public class AxEntityTypeDefn
             Name = (string)entityElement.Attribute("Name")!,
             Keys = entityElement.Element(edm + "Key")?.Elements(edm + "PropertyRef")
                 .Select(e => (string)e.Attribute("Name")!).ToArray() ?? [],
-            Properties = entityElement.Elements(edm + "Property").Select(AxEntityTypePropertyDefn.Parse).ToArray(),
+            Properties = entityElement.Elements(edm + "Property").Select(AxEntityTypePropertyDefn.Parse).ToArray<IPropertyDefn>(),
             Annotations = entityElement.Elements(edm + "Annotation").Select(AxEntityTypeAnnotationDefn.Parse).ToArray()
         });
     }
