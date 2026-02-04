ff313d17073d3251a5aa91b19738891bcebbaf0e
Josh Brunton <josh.brunton@proton.me>
2026-02-04 13:18:13 +0000

    Make -1 and -2 exclusive

--- src/Cli/Options/LogToStdErrOption.cs
+++ src/Cli/Options/LogToStdErrOption.cs
@@ -1,4 +1,5 @@
 ﻿using System.CommandLine;
+using System.CommandLine.Parsing;
 
 namespace Dev.JoshBrunton.DynamicsEndpointDiscovery.Cli.Options;
 
@@ -7,5 +8,19 @@ internal class LogToStdErrOption : Option<bool>
     public LogToStdErrOption() : base("--log-to-stderr", "-2")
     {
         Description = "Send all logs to the error stream, regardless of their log level.";
+        Validators.Add(arg =>
+        {
+            if (!arg.GetValue(this) || arg.Parent is not CommandResult cr)
+            {
+                return;
+            }
+
+            if (cr.Children.OfType<OptionResult>()
+                    .FirstOrDefault(o => o.Option is LogToStdOutOption)
+                    ?.GetValueOrDefault<bool>() == true)
+            {
+                arg.AddError($"{Name} can't be used when also using {new LogToStdOutOption().Name}");
+            }
+        });
     }
 }
--- src/Cli/Options/LogToStdOutOption.cs
+++ src/Cli/Options/LogToStdOutOption.cs
@@ -1,4 +1,5 @@
 ﻿using System.CommandLine;
+using System.CommandLine.Parsing;
 
 namespace Dev.JoshBrunton.DynamicsEndpointDiscovery.Cli.Options;
 
@@ -7,5 +8,20 @@ internal class LogToStdOutOption : Option<bool>
     public LogToStdOutOption() : base("--log-to-stdout", "-1")
     {
         Description = "Send all logs to the output stream, regardless of their log level.";
+
+        Validators.Add(arg =>
+        {
+            if (!arg.GetValue(this) || arg.Parent is not CommandResult cr)
+            {
+                return;
+            }
+
+            if (cr.Children.OfType<OptionResult>()
+                    .FirstOrDefault(o => o.Option is LogToStdErrOption)
+                    ?.GetValueOrDefault<bool>() == true)
+            {
+                arg.AddError($"{Name} can't be used when also using {new LogToStdErrOption().Name}"); 
+            }
+        });
     }
 }
