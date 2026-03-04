686d6d2ca0f134b5c4102312c7ab28c1f4444c1e
Josh Brunton <josh.brunton@proton.me>
2026-03-04 10:44:47 +0000

    Add tests for CultureService

---
+++ test/Rcl.Tests/Tests/Services/CultureServiceTests.cs
@@ -0,0 +1,22 @@
+﻿using Dev.JoshBrunton.DynamicsEndpointDiscovery.Rcl.Enums;
+using Dev.JoshBrunton.DynamicsEndpointDiscovery.Rcl.Services;
+
+namespace Dev.JoshBrunton.DynamicsEndpointDiscovery.Tests.Rcl.Tests.Tests.Services;
+
+[TestClass]
+[DoNotParallelize]
+public class CultureServiceTests
+{
+    public static IEnumerable<object[]> GetKnownCultures()
+    {
+        return Enum.GetValues<KnownCultures>().Select(value => (object[])[value]);
+    }
+
+    [TestMethod]
+    [DynamicData(nameof(GetKnownCultures))]
+    public void KnownCultures_ShouldResolve(KnownCultures culture)
+    {
+        var sut = new CultureService();
+        sut.SetCulture(culture);
+    }
+}
