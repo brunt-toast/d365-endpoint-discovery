e21bdb87b177ebcd9bb4b8d905e8e5eea3371cd3
Josh Brunton <josh.brunton@proton.me>
2026-03-26 11:13:22 +0000

    Update AcceptThumbprintOption description for clarity

--- src/Cli/Options/AcceptThumbprintOption.cs
+++ src/Cli/Options/AcceptThumbprintOption.cs
@@ -7,6 +7,6 @@ internal class AcceptThumbprintOption : Option<string>
     public AcceptThumbprintOption() : base("--accept-thumbprint")
     {
         Description = "Acceptable thumbprint for X.509 certificate when validating SSL connections. " +
-                      "If unset, certificates trusted by the system will be considered valid.";
+                      "This will be considered valid in addition to any certificates considered valid by the system.";
     }
 }
