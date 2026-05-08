14b8cd9528b21ef8e48c44d93c798bea66406bb0
Josh Brunton <josh.brunton@proton.me>
2026-05-08 11:20:58 +0100

    Localise AuthKind values

--- src/Rcl/Services/CultureService.cs
+++ src/Rcl/Services/CultureService.cs
@@ -1,10 +1,18 @@
 ﻿using Dev.JoshBrunton.DynamicsEndpointDiscovery.Rcl.Enums;
 using System.Globalization;
+using Microsoft.Extensions.Localization;
 
 namespace Dev.JoshBrunton.DynamicsEndpointDiscovery.Rcl.Services;
 
 internal class CultureService : ICultureService
 {
+    private readonly IServiceProvider _services;
+
+    public CultureService(IServiceProvider services)
+    {
+        _services = services;
+    }
+
     public void SetCulture(KnownCultures culture)
     {
         string cultureKey = culture switch
@@ -22,9 +30,16 @@ internal class CultureService : ICultureService
         CultureInfo.DefaultThreadCurrentCulture = cultureInfo;
         CultureInfo.DefaultThreadCurrentUICulture = cultureInfo;
     }
+
+    public string LocaliseEnum<TEnum,TResources>(TEnum value) where TEnum : struct, Enum
+    {
+        string? localised = _services.GetService<IStringLocalizer<TResources>>()?[value.ToString()].Value;
+        return string.IsNullOrWhiteSpace(localised) ? value.ToString() : localised;
+    }
 }
 
 internal interface ICultureService
 {
     void SetCulture(KnownCultures culture);
+    string LocaliseEnum<TEnum, TResources>(TEnum value) where TEnum : struct, Enum;
 }
